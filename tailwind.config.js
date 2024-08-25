49255381f75daed32ca7e3bed162899d024858da
parikshit2023360554 <singhparikshit2004@gmail.com>
2024-08-25 05:52:25 +0530

    file added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js}',
-  ],
+  content: ["./src/**/*.{html,js}"],
+  content: ["./chatgpt/**/*.{html,js}"],
   theme: {
     extend: {},
   },
