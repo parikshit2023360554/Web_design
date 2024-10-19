b34e24df1918c485a18f683446946bf2f353f6d7
parikshit2023360554 <singhparikshit2004@gmail.com>
2024-10-19 19:28:54 +0530

    file added

--- appEmial/script.js
+++ appEmial/script.js
@@ -1,19 +1,51 @@
 let users = [];
+let alert = document.getElementById('alert');
+
+
+
+
 
 function adduser(){
     let name = document.getElementById('name');
     let email = document.getElementById('email');
-
-
     let user = {
         name: name.value,
         email: email.value
     }
 
-    users.push(user);
+    let userExist = checkuser(email.value);
+    if(userExist == false){
+        users.push(user)
+        alert.classList.add('success');
+        alert.innerText = "user added ";
+        removeAlert();
+    }
+    else{
+        alert.classList.add('danger');
+        alert.innerText = 'email already added';
+        removeAlert();
+    }
+   
     console.log(users);
     renderuser();
 }
+
+
+
+
+
+function removeAlert(){
+    setTimeout(()=>{
+        alert.classList.remove('success' , 'danger');
+        alert.innerText = null; 
+    },2000)
+    
+}
+
+
+
+
+
 
 let uperdiv = document.getElementById('uperdisplay');
 function renderuser(){
@@ -31,4 +63,14 @@ function renderuser(){
         div.appendChild(name);
         div.appendChild(email);
     })
+}
+
+
+
+
+function checkuser(email){
+    let user = users.filter((user)=>{
+        return user.email == email ; 
+    })
+    return user.length > 0 ? true : false ; 
 }
